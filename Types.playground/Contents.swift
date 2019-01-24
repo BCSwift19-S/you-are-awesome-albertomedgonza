31047e4b40b964bbf49b8fb6bf603ce45b7c93aa
Alberto Medina <albertomedina@MacBook-Pro-8.local>
2019-01-24 16:40:54 -0500

    2.11 Complete

---
+++ Types.playground/Contents.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var myInt = 30000
+var myDouble = 30000.0
+var myBool = true
+
+var greeting = "Hello"
+var name = "Alberto"
+var name2 = "Alberto"
+
+var greetingString = greeting + name
+print(greetingString)
+
+//var kilometers: Double = 5 declaration
+var kilometers = 5
+var kmInMiles = 1.609
+
+//greetingString = greetingString + kilometers
+var howMuchIRan = Double(kilometers) / kmInMiles
+
+var isEqual = (name == name2)
+
+var myWeight = 170
+var percentGoal = 0.10
+
+var weightToLose = Double(myWeight) * percentGoal
+var weightMessage = "My Weight is currently" + String(myWeight)
+
+var oneEighth = 1.0/8.0
+var newWeightLossGoal = Double(myWeight) * oneEighth
+
+Double("170 LBS")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
